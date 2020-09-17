caa2dc5a42d89c1deb3c036d1c63f09138b84adb
Natalia-Run <70914248+Natalia-Run@users.noreply.github.com>
2020-09-18 00:50:51 +0500

    Добавление файла с программой в репозиторий

---
+++ Ceasar's Caesar.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int k = 2;
+	const int n = 33;
+	char string[] = "Булочка"
+		"с"
+		"корицей"
+		"лежит"
+		"на"
+		"самой"
+		"верхней"
+		"полке";
+	cout << string;
+	for (int i = 0; i < strlen(string); i++)
+	{
+		string[i] = (string[i] + k) % (n + __toascii('А'));
+	}
+	cout << string;
+	for (int i = 0; i < strlen(string); i++)
+	{
+		string[i] = (string[i] - k - (n + __toascii('А'))) % (n + __toascii('А'));
+	}
+	cout << string;
+	return 0;
+}
