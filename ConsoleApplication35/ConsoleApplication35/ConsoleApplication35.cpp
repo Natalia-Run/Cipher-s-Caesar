87b76c5da5448cacce5aa9294650334664f56618
Natalia-Run <70914248+Natalia-Run@users.noreply.github.com>
2020-09-19 18:23:16 +0500

    Добавлен проект Visual Studio

---
+++ ConsoleApplication35/ConsoleApplication35/ConsoleApplication35.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int k = 2;
+	const int n = 33;
+	char string[] = "Булочка "
+		"с "
+		"корицей "
+		"лежит "
+		"на "
+		"самой "
+		"верхней "
+		"полке ";
+	cout << "Строка до шифрования:" << endl;
+	cout << string << endl;
+	for (int i = 0; i < strlen(string); i++)
+	{
+		string[i] = (string[i] + k) % (n + __toascii('А'));
+	}
+	cout << '\n' << "Строка после шифрования:" << endl;
+	cout << string << endl;
+	for (int i = 0; i < strlen(string); i++)
+	{
+		string[i] = (string[i] - k - (n + __toascii('А'))) % (n + __toascii('А'));
+	}
+	cout << '\n' << "Расшифрованная строка:" << endl;
+	cout << string << endl;
+	return 0;
+}
