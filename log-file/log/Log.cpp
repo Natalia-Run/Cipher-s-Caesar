4e2cb6e054d86cd0735817eba5505df718acdff4
Natalia-Run <70914248+Natalia-Run@users.noreply.github.com>
2020-11-01 12:58:29 +0500

    Добавление log-файла

---
+++ log-file/log/Log.cpp
@@ -0,0 +1,31 @@
+﻿
+#ifndef _LOGGER_H_
+#define _LOGGER_H_
+
+#include "Cipher-s-Caesar.h"
+#include <direct.h>
+
+class Logger
+{
+	std::string logFileName; 
+	std::ofstream outLog; 
+
+public:
+
+	Logger(std::string logFileName);
+	
+	Logger();
+
+	char* getCurrentDateAndTime(char* currentDateAndTimeStr);
+
+
+	void startLoging();
+
+	void endLoging();
+
+	void writeLog(const char* fileName, bool infected, char* signatureName);
+
+	std::vector<std::string> GetAllSuspectedFiles();
+};
+
+#endif
